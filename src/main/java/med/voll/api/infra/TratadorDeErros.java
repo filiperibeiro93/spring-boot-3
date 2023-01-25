179c59a6fd670e7447ab481cc9f04f42602b4901
Filipe Ribeiro <facribeiro.93@gmail.com>
2023-01-25 09:26:02 -0300

    tratando erro 400 e de sql

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,8 +2,13 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -12,5 +17,25 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).collect(Collectors.toList()));
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity tratarErroSql(SQLIntegrityConstraintViolationException e) {
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    private record DadosErroValidacao(
+            String campo,
+            String mensagem
+    ) {
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
